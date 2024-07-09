953af5f3582188e3d91307d305e4a3d3c0563e5f
Clarisse Otmani <clarisseotm@gmail.com>
2024-07-09 11:45:18 +0200

    Exercice completed

--- src/main/java/fr/afpa/HashMapApp.java
+++ src/main/java/fr/afpa/HashMapApp.java
@@ -1,6 +1,7 @@
 package fr.afpa;
 
 import java.util.HashMap;
+import java.util.Map;
 
 //TODO Faites l'import de java HashMap
 
@@ -15,47 +16,67 @@ public class HashMapApp {
 
 
         // Instanciez une nouvelle HashMap sur le theme de votre choix
-        HashMap<String, String> skate = new HashMap<>();
+        HashMap<String, String> superHero= new HashMap<>();
 
         // Ajoutez des paires clé/valeur à votre HashMap - au moins 5 paires
         // skate.
-
+superHero.put("Name ", "Green Arrow - Olivier Queen");
+superHero.put("Age ", "30" );
+superHero.put("Outfit ", "Green cap" );
+superHero.put("Arme ", "Arc" );
+superHero.put("Best Friend ", "Felicity Smoack");
         // Récupérez la valeur d'une de vos paires
         
+        System.out.println(superHero.get("Age"));
 
         //  Vérifiez si une de vos clés est contenue dans la collection
-       
+       System.out.println(superHero.containsKey("Age "));//true
+       System.out.println(superHero.containsKey("Animal"));//false
 
         //  Vérifiez si une de vos valeurs est contenue dans la collection
-       
+        
+       System.out.println(superHero.containsValue("Arc"));//true
+       System.out.println(superHero.containsValue("Doberman"));//false
 
         //  Ajoutez une paire clé/valeur à votre HashMap
-        
+        superHero.put("Lover ", "Black Canary - Dinah Laurel Lance ");
 
         //  Utilisez une méthode pour trouver le nombre de paires (la longueur de votre HashMap) et affichez la en console
-        
+        System.out.println(superHero.size());
 
         //  Affichez toutes les clés de votre HashMap (avec une boucle for each & avec une méthode de HashMap)
-        
+        for (String key : superHero.keySet()) {
+            System.out.println("keys is:"  + key);
+            
+        }
 
-        // TODO Affichez toutes les valeurs de votre HashMap (avec une boucle for each & avec une méthode de HashMap)
-       
+        // Affichez toutes les valeurs de votre HashMap (avec une boucle for each & avec une méthode de HashMap)
+       for (String values : superHero.values()) {
+        System.out.println("values is : " + values);
+       }
 
 
-        // TODO Affichez toutes les clés/valeurs de votre HashMap (avec une boucle for each & avec une méthode de HashMap)
-        
+        // Affichez toutes les clés/valeurs de votre HashMap (avec une boucle for each & avec une méthode de HashMap)
+        for (Map.Entry<String, String> entry : superHero.entrySet()) {
+            
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println("Clé: " + key + ", Valeur: " + value);
+        }
 
-        // TODO Supprimez une de vos paires
-       
+        // Supprimez une de vos paires
+       superHero.remove("Age");
+       System.out.println(superHero);
 
-        // TODO Vérifiez si votre HashMap est vide
-       
+        // Vérifiez si votre HashMap est vide
+       System.out.println(superHero.isEmpty());
 
-        // TODO Supprimez tous les éléments de votre HashMap
-      
+        // Supprimez tous les éléments de votre HashMap
+      superHero.clear();
+      System.out.println(superHero);
 
-        // TODO Refaites la vérification
-    
+        // Refaites la vérification
+    System.out.println(superHero.isEmpty());
 
     }
 }
